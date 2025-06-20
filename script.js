37b0edcb728f8ec6bce0c6b9a2d8ed694170a3cd
Akshayasree23 <140528626+Akshayasree23@users.noreply.github.com>
2025-06-20 18:19:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	if (s2 === "") return 0; // per JS behavior, empty substring is found at index 0
+  return s1.toLowerCase().indexOf(s2.toLowerCase());
+
 }
 
 // Please do not change the code below
